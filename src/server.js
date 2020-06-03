391504ae12d9b5869e1eb28959fe892c09e85f9e
kylegibson15 <kylegibson15@gmail.com>
2020-06-02 22:16:29 -0600

    setHeader

--- src/server.js
+++ src/server.js
@@ -12,7 +12,14 @@ const app = express();
 
 require('dotenv').config();
 const apiPort = process.env.PORT || 3001;
+router.get('/api', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
+  res.send('cors problem fixed:)');
+});
 app.use(cors({origin: 'http://localhost:3001'}))
 app.use(bodyParser.json())
 
