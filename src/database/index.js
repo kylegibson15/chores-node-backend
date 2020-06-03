0471e97f3567f7595bdbb001052fd9e97f0808cb
kylegibson15 <kylegibson15@gmail.com>
2020-06-02 21:17:06 -0600

    move to src folder

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chores', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
